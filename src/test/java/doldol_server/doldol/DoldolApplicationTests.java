8b9a1397b634d3154eff8591820d2d2462f3c80c
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-05-11 15:17:33 +0900

    init: 스프링 환경설정

---
+++ src/test/java/doldol_server/doldol/DoldolApplicationTests.java
@@ -0,0 +1,13 @@
+package doldol_server.doldol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoldolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
