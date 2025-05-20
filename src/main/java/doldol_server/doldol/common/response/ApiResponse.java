7a2ea76e478e4608dd9340f280fa67b48cbd4ee2
cherryiJuice <chldlwn711@gmail.com>
2025-05-20 23:39:56 +0900

    feat: #9 공통 응답 포맷 추가

---
+++ src/main/java/doldol_server/doldol/common/response/ApiResponse.java
@@ -0,0 +1,32 @@
+package doldol_server.doldol.common.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+	@Schema(name = "성공 응답에 대한 데이터")
+	private final T data;
+	@Schema(name = "HTTP 상태코드", example = "200")
+	private final int status;
+	@Schema(name = "요청 성공 메세지", example = "OK")
+	private final String message;
+
+	private ApiResponse(T data, int status, String message) {
+		this.data = data;
+		this.status = status;
+		this.message = message;
+	}
+
+	public static <T> ApiResponse<T> ok(T data) {
+		return new ApiResponse<>(data, 200, "OK");
+	}
+
+	public static <T> ApiResponse<T> created(T data) {
+		return new ApiResponse<>(data, 201, "CREATED");
+	}
+
+	public static ApiResponse<Void> noContent() {
+		return new ApiResponse<>(null, 204, "NO_CONTENT");
+	}
+}
