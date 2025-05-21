1b678a888152eaec3e3f5a73c9992c022a68d5a9
cherryiJuice <chldlwn711@gmail.com>
2025-05-21 11:43:55 +0900

    feat: #9 swagger 설정 추가

---
+++ src/main/java/doldol_server/doldol/common/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package doldol_server.doldol.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"local", "dev"})
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Doldol Project API Document")
+				.description("Doldol 서버 API 명세서입니다.")
+				.version("v1.0.0")
+			).components(authComponent());
+	}
+
+	private Components authComponent() {
+		return new Components().addSecuritySchemes("jwt-token",
+			new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization")
+				.scheme("bearer")
+				.bearerFormat("JWT"));
+	}
+}
+
