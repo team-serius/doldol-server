d62ed43ae48c34a91c25278aadd77acee33adbba
Hoonssac <Hoonssac.dev@gmail.com>
2025-05-22 13:36:10 +0900

    feat: #12 add compaint entity

---
+++ src/main/java/doldol_server/doldol/user/Role.java
@@ -0,0 +1,13 @@
+package doldol_server.doldol.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+
+    private final String role;
+}
