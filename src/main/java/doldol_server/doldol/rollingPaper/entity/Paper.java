bb51a0a8dfe2c3c7af86fd429387128c3427baa4
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-05-22 13:38:28 +0900

    feat/#12: domain 생성 - message와 paper

---
+++ src/main/java/doldol_server/doldol/rollingPaper/entity/Paper.java
@@ -0,0 +1,48 @@
+package doldol_server.doldol.rollingPaper.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "PAPER")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Paper {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "paper_id")
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "open_date", nullable = false)
+	private LocalDateTime openDate;
+
+	@Column(name = "link", nullable = false)
+	private String link;
+
+	@Column(name = "participants_count")
+	private Long participantsCount;
+
+	@Column(name = "message_count")
+	private Long messageCount;
+
+	@Column(name = "is_deleted")
+	private boolean isDeleted = false;
+
+	@Column(name = "created_date", nullable = false)
+	private LocalDateTime createDate;
+
+	@Column(name = "modified_date")
+	private LocalDateTime modifiedDate;
+}
