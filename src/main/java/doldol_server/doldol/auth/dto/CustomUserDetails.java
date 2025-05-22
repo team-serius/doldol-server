8308c83c4281bab058be289e50f42d375b07f82d
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 18:07:25 +0900

    feat: #8 login with custom security filter

---
+++ src/main/java/doldol_server/doldol/auth/dto/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package doldol_server.doldol.auth.dto;
+
+import doldol_server.doldol.user.entity.User;
+import java.util.ArrayList;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(() -> user.getRole().name());
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+}
