6bf6e6cff4150052cf0e24741945272fb9d76a3c
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 18:05:52 +0900

    feat: #8 add constant class

---
+++ src/main/java/doldol_server/doldol/common/constants/CookieConstant.java
@@ -0,0 +1,7 @@
+package doldol_server.doldol.common.constants;
+
+public class CookieConstant
+{
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "Refresh-Token";
+    public static final int REFRESH_EXPIRATION_DELETE = 0;
+}
