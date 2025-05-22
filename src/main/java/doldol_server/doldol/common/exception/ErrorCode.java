7b7f28eaf0041beb4f647641742244d834caf6be
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 18:05:00 +0900

    refactor: #8 implement extensible error code system with domain-specific enums

--- src/main/java/doldol_server/doldol/common/exception/ErrorCode.java
+++ src/main/java/doldol_server/doldol/common/exception/ErrorCode.java
@@ -2,18 +2,9 @@ package doldol_server.doldol.common.exception;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+public interface ErrorCode {
 
-@Getter
-@RequiredArgsConstructor
-public enum ErrorCode {
-	/**
-	 * 데이터 유효성 검사 실패
-	 */
-	INVALID_VALUE(HttpStatus.BAD_REQUEST, "D-001", "잘못된 요청입니다.");
-
-	private final HttpStatus httpStatus;
-	private final String code;
-	private final String message;
+	HttpStatus getHttpStatus();
+	String getCode();
+	String getMessage();
 }
