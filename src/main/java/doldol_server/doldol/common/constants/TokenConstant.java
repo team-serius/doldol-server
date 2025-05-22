b35ca9e92def07a42ca0580f2bbf912d0490af43
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 18:05:52 +0900

    feat: #8 add constant class

---
+++ src/main/java/doldol_server/doldol/common/constants/TokenConstant.java
@@ -0,0 +1,10 @@
+package doldol_server.doldol.common.constants;
+
+public class TokenConstant {
+
+    public static final String BEARER_FIX = "Bearer ";
+    public static final int MINUTE_IN_MILLISECONDS = 60 * 1000;
+    public static final long DAYS_IN_MILLISECONDS = 24 * 60 * 60 * 1000L;
+    public static final int ACCESS_TOKEN_EXPIRATION_MINUTE = 30;
+    public static final int REFRESH_TOKEN_EXPIRATION_DAYS = 14;
+}
