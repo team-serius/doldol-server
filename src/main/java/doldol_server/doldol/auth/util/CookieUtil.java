8ef0d34782469667273fb398f0a2ca0873e0f34f
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 18:22:29 +0900

    feat: #8 add jwt provider and jwt validation filter

---
+++ src/main/java/doldol_server/doldol/auth/util/CookieUtil.java
@@ -0,0 +1,29 @@
+package doldol_server.doldol.auth.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseCookie;
+
+public final class CookieUtil {
+
+    public static ResponseCookie createCookie(String name, String value, long cookieExpiration) {
+        return ResponseCookie.from(name, value)
+                .maxAge(cookieExpiration)
+                .path("/")
+                .sameSite("Strict")
+                .httpOnly(true)
+                .build();
+    }
+
+    public static Cookie findCookieByName(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+}
