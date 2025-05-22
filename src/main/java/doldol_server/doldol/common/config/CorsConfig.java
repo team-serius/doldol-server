c2d4f9cfda74fd31e91ce369776620a8b4410f16
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-22 17:46:00 +0900

    feat: #8 add cors, jpa, redis config

---
+++ src/main/java/doldol_server/doldol/common/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package doldol_server.doldol.common.config;
+
+import static doldol_server.doldol.common.constants.CorsConstant.ALLOWED_CREDENTIALS;
+import static doldol_server.doldol.common.constants.CorsConstant.ALLOWED_HEADERS;
+import static doldol_server.doldol.common.constants.CorsConstant.ALLOWED_METHODS;
+import static doldol_server.doldol.common.constants.CorsConstant.ALLOWED_ORIGINS;
+import static doldol_server.doldol.common.constants.CorsConstant.MAX_AGE;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Collections.singletonList(ALLOWED_ORIGINS));
+        configuration.setAllowedMethods(Collections.singletonList(ALLOWED_METHODS));
+        configuration.setAllowCredentials(ALLOWED_CREDENTIALS);
+        configuration.setAllowedHeaders(Collections.singletonList(ALLOWED_HEADERS));
+        configuration.setMaxAge(MAX_AGE);
+        configuration.addExposedHeader(HttpHeaders.SET_COOKIE);
+        configuration.addExposedHeader(HttpHeaders.AUTHORIZATION);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
+}
