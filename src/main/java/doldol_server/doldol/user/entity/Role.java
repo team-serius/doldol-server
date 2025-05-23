22e9d502276f94df56d0a16cbfee1ac34180c5f6
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-23 14:04:23 +0900

    refactor: #8 apply naver convention style

--- src/main/java/doldol_server/doldol/user/entity/Role.java
+++ src/main/java/doldol_server/doldol/user/entity/Role.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+	ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
 
-    private final String role;
+	private final String role;
 }
