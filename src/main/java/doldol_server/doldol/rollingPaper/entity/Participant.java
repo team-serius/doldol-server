2c95891333165204aa0121cb46357840a23485e3
Hoonssac <Hoonssac.dev@gmail.com>
2025-05-23 13:27:59 +0900

    feat: #12 delete admin column in Participant

--- src/main/java/doldol_server/doldol/rollingPaper/entity/Participant.java
+++ src/main/java/doldol_server/doldol/rollingPaper/entity/Participant.java
@@ -1,7 +1,6 @@
 package doldol_server.doldol.rollingPaper.entity;
 
 import doldol_server.doldol.user.User;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -27,22 +26,18 @@ public class Participant {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	// 참여자 아이디
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	// 롤링페이퍼 아이디
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "paper_id")
 	private Paper paper;
 
-	// 참여자가 쓴 메시지 아이디
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "meesage_id")
 	private Message message;
 
-	// 방장 여부
 	@Column(name = "is_master", nullable = false)
 	private boolean isMaster;
 }
