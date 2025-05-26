90a6c8908b607fa0b88a0d5598929cda781011f1
cherryiJuice <chldlwn711@gmail.com>
2025-05-26 21:38:12 +0900

    refactor: #11 인증 필요없는 부분 삭제

--- src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
+++ src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
 		"/v3/api-docs/**",
 		"/swagger-resources/**",
 		"/webjars/**",
-		"/auth/join"
+		"/auth/join",
+		"/papers/invite"
 	};
 
 	private final TokenProvider tokenProvider;
