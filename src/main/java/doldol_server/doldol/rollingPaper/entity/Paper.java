f195a34974c8dde31e0979699aeac4f8a9e4c7c4
cherryiJuice <chldlwn711@gmail.com>
2025-05-26 15:44:29 +0900

    refactor: #12 베이스 엔티티 extends & @Table 삭제

--- src/main/java/doldol_server/doldol/rollingPaper/entity/Paper.java
+++ src/main/java/doldol_server/doldol/rollingPaper/entity/Paper.java
@@ -2,21 +2,20 @@ package doldol_server.doldol.rollingPaper.entity;
 
 import java.time.LocalDateTime;
 
+import doldol_server.doldol.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Table(name = "PAPER")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Paper {
+public class Paper extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "paper_id")
@@ -39,10 +38,4 @@ public class Paper {
 
 	@Column(name = "is_deleted")
 	private boolean isDeleted = false;
-
-	@Column(name = "created_date", nullable = false)
-	private LocalDateTime createDate;
-
-	@Column(name = "modified_date")
-	private LocalDateTime modifiedDate;
 }
