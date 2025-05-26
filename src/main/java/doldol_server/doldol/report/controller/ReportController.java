1a6e91b26f0829f94288926d3d0a4aa3b90a650c
cherryiJuice <chldlwn711@gmail.com>
2025-05-26 17:45:18 +0900

    feat: #11 신고 관련 API 명세서

---
+++ src/main/java/doldol_server/doldol/report/controller/ReportController.java
@@ -0,0 +1,61 @@
+package doldol_server.doldol.report.controller;
+
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import doldol_server.doldol.auth.dto.CustomUserDetails;
+import doldol_server.doldol.common.response.ApiResponse;
+import doldol_server.doldol.report.dto.request.ReportRequest;
+import doldol_server.doldol.report.dto.response.ReportResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@Tag(name = "신고")
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+	@GetMapping
+	@Operation(
+		summary = "신고 내역 조회 API",
+		description = "신고 내역 조회",
+		security = {@SecurityRequirement(name = "jwt")})
+	public ApiResponse<List<ReportResponse>> getComplaints(
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		List<ReportResponse> response = null;
+		return ApiResponse.ok(response);
+	}
+
+	@GetMapping("/{id}")
+	@Operation(
+		summary = "신고 상세 조회 API",
+		description = "신고 상세 조회",
+		security = {@SecurityRequirement(name = "jwt")})
+	public ApiResponse<ReportResponse> getComplaint(
+		@PathVariable("id") Long reportId,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		ReportResponse response = null;
+		return ApiResponse.ok(response);
+	}
+
+	@PostMapping
+	@Operation(
+		summary = "신고 작성 API",
+		description = "신고",
+		security = {@SecurityRequirement(name = "jwt")})
+	public ApiResponse<ReportResponse> createMessage(
+		@RequestBody @Valid ReportRequest request,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		ReportResponse response = null;
+		return ApiResponse.created(response);
+	}
+}
---
+++ src/main/java/doldol_server/doldol/report/dto/request/ReportRequest.java
@@ -0,0 +1,28 @@
+package doldol_server.doldol.report.dto.request;
+
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(name = "ReportRequest: 신고 내역 요청 Dto")
+public record ReportRequest(
+	@NotNull(message = "메세지 ID는 필수입니다.")
+	@Schema(description = "메세지 ID", example = "1")
+	Long messageId,
+
+	@NotBlank(message = "메세지 제목은 필수입니다.")
+	@Schema(description = "제목", example = "신고합니다.")
+	String title,
+
+	@NotBlank(message = "메세지 내용은 필수입니다.")
+	@Schema(description = "내용", example = "김돌돌씨를 신고합니다.")
+	String content,
+
+	@NotBlank(message = "생성 날짜는 필수입니다.")
+	@NotNull
+	@Schema(description = "생성 날짜", example = "2025-05-26T11:44:30.327959")
+	LocalDateTime createdAt
+) {
+}
