5c827756130bbb92ba7d04f0ec93a1921a5236b7
cherryiJuice <chldlwn711@gmail.com>
2025-05-26 17:45:18 +0900

    feat: #11 신고 관련 API 명세서

---
+++ src/main/java/doldol_server/doldol/report/dto/response/ReportResponse.java
@@ -0,0 +1,24 @@
+package doldol_server.doldol.report.dto.response;
+
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "ReportResponse: 신고 내역 응답 Dto")
+public record ReportResponse(
+	@Schema(description = "메세지 ID", example = "1")
+	Long messageId,
+
+	@Schema(description = "제목", example = "신고합니다.")
+	String title,
+
+	@Schema(description = "내용", example = "김돌돌씨를 신고합니다.")
+	String content,
+
+	@Schema(description = "생성 날짜", example = "2025-05-26T11:44:30.327959")
+	LocalDateTime createdAt,
+
+	@Schema(description = "답변 여부", example = "false")
+	boolean isAnswered
+) {
+}
