ce17de63ce6f9a9660ed6e60f3ffe7c17c7d6fde
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-27 14:30:13 +0900

    feat: #41 join controller

---
+++ src/main/java/doldol_server/doldol/auth/dto/request/EmailCodeVerifyRequest.java
@@ -0,0 +1,14 @@
+package doldol_server.doldol.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailCodeVerifyRequest(@NotBlank(message = "이메일은 필수 입력값입니다.")
+									 @Email(message = "올바른 이메일 양식을 입력해주세요.")
+									 @Schema(description = "이메일", example = "doldol@test.com")
+									 String email,
+									 @NotBlank(message = "인증번호는 필수 입력값입니다.")
+									 @Schema(description = "인증번호", example = "123456")
+									 String code) {
+}
