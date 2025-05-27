adb46545bfb45ef12b2cdc11647fe4c066d686fa
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-27 10:40:15 +0900

    feat: #35 set oauth2 handler on security config

--- src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
+++ src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
@@ -7,7 +7,11 @@ import doldol_server.doldol.auth.filter.CustomUserLoginFilter;
 import doldol_server.doldol.auth.filter.JwtAuthenticationFilter;
 import doldol_server.doldol.auth.handler.CustomAccessDeniedHandler;
 import doldol_server.doldol.auth.handler.CustomAuthenticationEntryPoint;
+import doldol_server.doldol.auth.handler.CustomOAuth2FailureHandler;
+import doldol_server.doldol.auth.handler.CustomOAuth2SuccessHandler;
 import doldol_server.doldol.auth.jwt.TokenProvider;
+import doldol_server.doldol.auth.resolver.CustomOAuth2ParameterResolver;
+import doldol_server.doldol.auth.service.CustomOAuth2UserService;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 
@@ -31,19 +35,21 @@ import org.springframework.web.cors.CorsConfigurationSource;
 public class SecurityConfig {
 
 	private static final String[] WHITELIST = {
-		"/auth/login",
+		"/auth/**",
 		"/swagger-ui/**",
 		"/v3/api-docs/**",
 		"/swagger-resources/**",
 		"/webjars/**",
-		"/auth/join",
-		"/papers/invite"
 	};
 
 	private final TokenProvider tokenProvider;
 	private final ObjectMapper objectMapper;
 	private final Validator validator;
 	private final CorsConfigurationSource corsConfigurationSource;
+	private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+	private final CustomOAuth2FailureHandler customOAuth2FailureHandler;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomOAuth2ParameterResolver customOAuth2ParameterResolver;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager)
@@ -54,6 +60,16 @@ public class SecurityConfig {
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.oauth2Login(oauth2 -> oauth2
+				.authorizationEndpoint(authorization -> authorization
+					.authorizationRequestResolver(customOAuth2ParameterResolver)
+				)
+				.userInfoEndpoint(userInfo -> userInfo
+					.userService(customOAuth2UserService)
+				)
+				.successHandler(customOAuth2SuccessHandler)
+				.failureHandler(customOAuth2FailureHandler)
+			)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(WHITELIST).permitAll()
 				.anyRequest().authenticated())
