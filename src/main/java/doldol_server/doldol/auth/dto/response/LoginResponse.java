5844379e8e77f62ba50b617af6a44e2f409dcdb1
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-27 11:28:20 +0900

    refactor: #35 change class type to record

--- src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
+++ src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
@@ -2,19 +2,10 @@ package doldol_server.doldol.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+@Builder
 @Schema(name = "LoginResponse: 로그인 응답 Dto")
-public class LoginResponse {
-
+public record LoginResponse(
 	@Schema(description = "로그인 성공한 유저의 권한입니다.", example = "ADMIN")
-	private String role;
-
-	@Builder
-	public LoginResponse(String role) {
-		this.role = role;
-	}
-}
+	String role
+) {}
