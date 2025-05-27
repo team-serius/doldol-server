ca7cc7fba9aaadec710601ce43bfb32b72cb7362
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-27 10:38:02 +0900

    refactor: #35 change class name

--- src/main/java/doldol_server/doldol/auth/dto/request/LoginRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/LoginRequest.java
@@ -1,31 +1,20 @@
-package doldol_server.doldol.auth.dto;
+package doldol_server.doldol.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@Schema(name = "LoginReqDto: 로그인 요청 Dto")
-public class LoginReqDto {
+public record LoginRequest(
 
     @NotBlank(message = "아이디는 필수 입력값입니다.")
     @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 4~20자리여야 합니다.")
     @Schema(description = "아이디는 입력되어야 합니다. 아이디는 영어 소문자와 숫자만 사용하여 4~20자리입니다", example = "doldol1234")
-    private String id;
+    String id,
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$",
-            message = "비밀번호는 8~16자리로 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.")
+        message = "비밀번호는 8~16자리로 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.")
     @Schema(description = "비밀번호는 입력되어야 합니다. 비밀번호는 8~16자리 수 입니다. 영문 대소문자, 숫자, 특수문자를 포함합니다.", example = "doldol1234!")
-    private String password;
+    String password
 
-    @Builder
-    public LoginReqDto(String id, String password) {
-        this.id = id;
-        this.password = password;
-    }
-}
+) {}
--- src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
+++ src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
@@ -1,4 +1,4 @@
-package doldol_server.doldol.auth.dto;
+package doldol_server.doldol.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -7,14 +7,14 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@Schema(name = "LoginResDto: 로그인 응답 Dto")
-public class LoginResDto {
+@Schema(name = "LoginResponse: 로그인 응답 Dto")
+public class LoginResponse {
 
 	@Schema(description = "로그인 성공한 유저의 권한입니다.", example = "ADMIN")
 	private String role;
 
 	@Builder
-	public LoginResDto(String role) {
+	public LoginResponse(String role) {
 		this.role = role;
 	}
 }
