d3eaf362f0eb1a241a11990b423023cf1b6c7216
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-28 20:46:11 +0900

    fix: #43 delete validator error message because of login failure message

--- src/main/java/doldol_server/doldol/auth/filter/CustomUserLoginFilter.java
+++ src/main/java/doldol_server/doldol/auth/filter/CustomUserLoginFilter.java
@@ -1,34 +1,29 @@
 package doldol_server.doldol.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import doldol_server.doldol.auth.dto.request.LoginRequest;
 import doldol_server.doldol.auth.jwt.TokenProvider;
+import doldol_server.doldol.auth.util.ResponseUtil;
+import doldol_server.doldol.common.exception.AuthErrorCode;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
+
+import java.io.IOException;
 import java.util.Set;
+
 import org.springframework.security.authentication.AuthenticationManager;
 
 public class CustomUserLoginFilter extends CustomUsernamePasswordAuthenticationFilter {
 
-    private static final String LONGIN_URI = "/auth/login";
-    private final Validator validator;
+	private static final String LONGIN_URI = "/auth/login";
 
-    public CustomUserLoginFilter(AuthenticationManager authenticationManager,
-                                 TokenProvider tokenProvider,
-                                 ObjectMapper objectMapper,
-                                 Validator validator) {
-        super(authenticationManager, tokenProvider, objectMapper);
-        this.validator = validator;
-        this.setFilterProcessesUrl(LONGIN_URI);
-    }
+	public CustomUserLoginFilter(AuthenticationManager authenticationManager,
+		TokenProvider tokenProvider,
+		ObjectMapper objectMapper) {
+		super(authenticationManager, tokenProvider, objectMapper);
+		this.setFilterProcessesUrl(LONGIN_URI);
+	}
 
-    @Override
-    protected void validateLoginRequestDto(LoginRequest loginRequest) {
-        Set<ConstraintViolation<LoginRequest>> violations = validator.validate(loginRequest);
-
-        if (!violations.isEmpty()) {
-            String errorMessage = violations.iterator().next().getMessage();
-            throw new IllegalArgumentException(errorMessage);
-        }
-    }
 }
