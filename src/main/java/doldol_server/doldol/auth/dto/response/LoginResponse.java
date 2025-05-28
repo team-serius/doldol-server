a76e97d663bd3dac35e29258b38057fe08290437
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-28 20:48:47 +0900

    refactor: #43 return userId when login success

--- src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
+++ src/main/java/doldol_server/doldol/auth/dto/response/LoginResponse.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 @Builder
 @Schema(name = "LoginResponse: 로그인 응답 Dto")
 public record LoginResponse(
+	@Schema(description = "유저 식별 값입니다,", example = "1")
+	Long userId,
 	@Schema(description = "로그인 성공한 유저의 권한입니다.", example = "ADMIN")
 	String role
 ) {}
