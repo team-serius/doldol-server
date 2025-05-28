cfecb8093f2d5ed860fadb2639768b89b4de6617
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-28 16:09:40 +0900

    refactor: #43 delete social type on CustomUserDetails

--- src/main/java/doldol_server/doldol/auth/dto/OAuth2Response.java
+++ src/main/java/doldol_server/doldol/auth/dto/OAuth2Response.java
@@ -1,12 +1,9 @@
 package doldol_server.doldol.auth.dto;
 
-import doldol_server.doldol.user.entity.SocialType;
-
 public interface OAuth2Response {
 
 	String getSocialId();
 
 	String getEmail();
 
-	SocialType getSocialType();
 }
