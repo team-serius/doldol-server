ce279ff6269661e1387946a6f208061e913a1af6
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:03:18 +0900

    refactor: #53 delete unused class

--- src/main/java/doldol_server/doldol/auth/dto/response/VerifiableSignupResponse.java
+++
@@ -1,9 +0,0 @@
-package doldol_server.doldol.auth.dto.response;
-
-public interface VerifiableSignupResponse {
-    String getVerificationCode();
-    void initVerificationCode(String code);
-    void updateVerificationStatus();
-    boolean isVerified();
-    String getEmail();
-}
