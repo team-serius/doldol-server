bb70602a6e568eb56ef7f99a4c9c6a94cc9aaffd
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:25:38 +0900

    refactor: #53 add "request" explaination on dto

--- src/main/java/doldol_server/doldol/auth/dto/request/EmailCheckRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/EmailCheckRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(name = "EmailCheckRequest: 이메일 중복 검증 Dto")
+@Schema(name = "EmailCheckRequest: 이메일 중복 검증 요청 Dto")
 public record EmailCheckRequest(
 	@NotBlank(message = "이메일은 필수 입력값입니다.")
 	@Email(message = "올바른 이메일 양식을 입력해주세요.")
