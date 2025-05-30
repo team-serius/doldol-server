e8264d4117485a4b06c519c9ff1c056b93a310c5
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:13:45 +0900

    refactor: #53 add dto explaination

--- src/main/java/doldol_server/doldol/auth/dto/request/RegisterRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/RegisterRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+@Schema(name = "RegisterRequest: 자체 서비스 회원가입 요청 Dto")
 public record RegisterRequest(
 	@NotBlank(message = "아이디는 필수 입력값입니다.")
 	@Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 4~20자리여야 합니다.")
