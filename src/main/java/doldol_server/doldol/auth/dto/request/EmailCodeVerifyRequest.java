f502bc75fa1fe646863e6b4f2c6ee7955503ddbb
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:25:38 +0900

    refactor: #53 add "request" explaination on dto

--- src/main/java/doldol_server/doldol/auth/dto/request/EmailCodeVerifyRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/EmailCodeVerifyRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(name = "EmailCodeVerifyRequest: 이메일 인증 코드 검증 Dto")
+@Schema(name = "EmailCodeVerifyRequest: 이메일 인증 코드 검증 요청 Dto")
 public record EmailCodeVerifyRequest(@NotBlank(message = "이메일은 필수 입력값입니다.")
 									 @Email(message = "올바른 이메일 양식을 입력해주세요.")
 									 @Schema(description = "이메일", example = "doldol@test.com")
