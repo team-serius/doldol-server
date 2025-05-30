4de9308126e8cd7b17474cecfc06e12981b1decc
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:06:33 +0900

    refactor: #53 check duplicated info dto

---
+++ src/main/java/doldol_server/doldol/auth/dto/request/PhoneCheckRequest.java
@@ -0,0 +1,12 @@
+package doldol_server.doldol.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record PhoneCheckRequest(
+	@NotBlank(message = "휴대전화 번호는 필수 입력값입니다.")
+	@Pattern(regexp = "^010\\d{8}$", message = "전화번호는 010으로 시작하는 11자리 숫자여야 합니다.")
+	@Schema(description = "휴대전화 번호", example = "01012341234")
+	String phone) {
+}
