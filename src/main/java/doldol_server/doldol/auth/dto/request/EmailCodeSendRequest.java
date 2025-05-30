ff4af4e6fffde627fcc168e788b480f8fb7803f8
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-30 21:25:38 +0900

    refactor: #53 add "request" explaination on dto

--- src/main/java/doldol_server/doldol/auth/dto/request/EmailCodeSendRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/EmailCodeSendRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(name = "EmailCodeSendRequest: 이메일 인증 코드 전송 Dto")
+@Schema(name = "EmailCodeSendRequest: 이메일 인증 코드 전송 요청 Dto")
 public record EmailCodeSendRequest(@NotBlank(message = "이메일은 필수 입력값입니다.")
 								   @Email(message = "올바른 이메일 양식을 입력해주세요.")
 								   @Schema(description = "이메일", example = "doldol@test.com")
--- src/main/java/doldol_server/doldol/auth/dto/request/PhoneCheckRequest.java
+++ src/main/java/doldol_server/doldol/auth/dto/request/PhoneCheckRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-@Schema(name = "PhoneCheckRequest: 휴대폰 번호 중복 검증 Dto")
+@Schema(name = "PhoneCheckRequest: 휴대폰 번호 중복 검증 요청 Dto")
 public record PhoneCheckRequest(
 	@NotBlank(message = "휴대전화 번호는 필수 입력값입니다.")
 	@Pattern(regexp = "^010\\d{8}$", message = "전화번호는 010으로 시작하는 11자리 숫자여야 합니다.")
