d8f930634e3f1750b9abf1f67eb3854eab40d7ad
cherryiJuice <chldlwn711@gmail.com>
2025-06-01 21:37:29 +0900

    feat: #44 초대 조회 API 추가

--- src/main/java/doldol_server/doldol/rollingPaper/repository/PaperRepository.java
+++ src/main/java/doldol_server/doldol/rollingPaper/repository/PaperRepository.java
@@ -1,5 +1,7 @@
 package doldol_server.doldol.rollingPaper.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import doldol_server.doldol.rollingPaper.entity.Paper;
 
 @Repository
 public interface PaperRepository extends JpaRepository<Paper, Long> {
+	Optional<Paper> findByInvitationCode(String invitationCode);
 }
