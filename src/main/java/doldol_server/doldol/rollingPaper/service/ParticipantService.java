45318a972e7a2b69684a782d0dbb299e99a29345
cherryiJuice <chldlwn711@gmail.com>
2025-06-02 13:26:06 +0900

    feat: #44 롤링페이퍼 참여자 조회 API

--- src/main/java/doldol_server/doldol/rollingPaper/service/ParticipantService.java
+++ src/main/java/doldol_server/doldol/rollingPaper/service/ParticipantService.java
@@ -1,12 +1,18 @@
 package doldol_server.doldol.rollingPaper.service;
 
+import static doldol_server.doldol.common.exception.errorCode.PaperErrorCode.*;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import doldol_server.doldol.common.exception.CustomException;
+import doldol_server.doldol.rollingPaper.dto.response.ParticipantResponse;
 import doldol_server.doldol.rollingPaper.entity.Paper;
 import doldol_server.doldol.rollingPaper.entity.Participant;
+import doldol_server.doldol.rollingPaper.repository.PaperRepository;
 import doldol_server.doldol.rollingPaper.repository.ParticipantRepository;
-import doldol_server.doldol.user.entity.User;
 import doldol_server.doldol.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +23,7 @@ public class ParticipantService {
 
 	private final ParticipantRepository participantRepository;
 	private final UserService userService;
+	private final PaperRepository paperRepository;
 
 	@Transactional
 	public void addUser(Long userId, Paper paper, boolean isMaster) {
@@ -30,8 +37,17 @@ public class ParticipantService {
 		participantRepository.save(participant);
 	}
 
-	public boolean existUserInPaper(Long userId, Paper paper) {
-		User user = userService.getById(userId);
-		return participantRepository.existsByUserAndPaper(user, paper);
+	public boolean existUserInPaper(Long userId, Long paperId) {
+		return participantRepository.existsByUserAndPaper(userId, paperId);
+	}
+
+	public List<ParticipantResponse> getParticipants(Long paperId, Long userId) {
+		Paper paper = paperRepository.findById(paperId).orElseThrow(() -> new CustomException(PAPER_NOT_FOUND));
+
+		if(!existUserInPaper(userId, paper.getId())) {
+			throw new CustomException(PARTICIPANT_NOT_FOUND);
+		}
+
+		return participantRepository.findAllByPaperAndUserExceptMe(paperId, userId);
 	}
 }
