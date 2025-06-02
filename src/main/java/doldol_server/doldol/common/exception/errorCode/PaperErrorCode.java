85c515c46981be091fd8297d21e5e526957496b1
cherryiJuice <chldlwn711@gmail.com>
2025-06-02 13:26:06 +0900

    feat: #44 롤링페이퍼 참여자 조회 API

--- src/main/java/doldol_server/doldol/common/exception/errorCode/PaperErrorCode.java
+++ src/main/java/doldol_server/doldol/common/exception/errorCode/PaperErrorCode.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public enum PaperErrorCode implements ErrorCode {
 	PAPER_NOT_FOUND(HttpStatus.NOT_FOUND, "P-001", "롤링페이퍼를 찾을 수 없습니다."),
 	PARTICIPANT_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "P-002", "롤링페이퍼에 사용자가 이미 존재합니다."),
-
+	PARTICIPANT_NOT_FOUND(HttpStatus.NOT_FOUND, "P-003", "롤링페이퍼에서 사용자를 찾을 수 없습니다."),
 	;
 
 	private HttpStatus httpStatus;
