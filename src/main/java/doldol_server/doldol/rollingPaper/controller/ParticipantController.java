3ab032619d0888cb2fd893c2973b811fd8278e2b
cherryiJuice <chldlwn711@gmail.com>
2025-06-02 13:26:06 +0900

    feat: #44 롤링페이퍼 참여자 조회 API

--- src/main/java/doldol_server/doldol/rollingPaper/controller/ParticipantController.java
+++ src/main/java/doldol_server/doldol/rollingPaper/controller/ParticipantController.java
@@ -12,24 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 import doldol_server.doldol.auth.dto.CustomUserDetails;
 import doldol_server.doldol.common.response.ApiResponse;
 import doldol_server.doldol.rollingPaper.dto.response.ParticipantResponse;
+import doldol_server.doldol.rollingPaper.service.ParticipantService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "참여자")
 @RestController
 @RequestMapping("/participants")
+@RequiredArgsConstructor
 public class ParticipantController {
+
+	private final ParticipantService participantService;
 
 	@GetMapping("/{id}")
 	@Operation(
 		summary = "롤링페이퍼 참여자 조회 API",
 		description = "롤링페이퍼 참여자 조회",
 		security = {@SecurityRequirement(name = "jwt")})
-	public ResponseEntity<ApiResponse<List<ParticipantResponse>>> getComplaints(
+	public ResponseEntity<ApiResponse<List<ParticipantResponse>>> getParticipants(
 		@PathVariable("id") Long paperId,
 		@AuthenticationPrincipal CustomUserDetails userDetails) {
-		List<ParticipantResponse> response = null;
+		List<ParticipantResponse> response = participantService.getParticipants(paperId, userDetails.getUserId());
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 }
