3e7025471d08a2312ef3f2b145fcb0571edd26ea
cherryiJuice <chldlwn711@gmail.com>
2025-06-03 23:50:03 +0900

    refactor: #44 롤링페이퍼 총 개수 추가 및 필요없는 커서 페이징 응답 포맷 삭제

---
+++ src/main/java/doldol_server/doldol/rollingPaper/dto/response/PaperListResponse.java
@@ -0,0 +1,16 @@
+package doldol_server.doldol.rollingPaper.dto.response;
+
+import doldol_server.doldol.common.dto.CursorPage;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "PaperListResponse: 롤링페이퍼 리스트 응답 Dto")
+public record PaperListResponse(
+	@Schema(description = "총 페이퍼 수", example = "8")
+	int paperCount,
+
+	CursorPage<PaperResponse> rollingPaper
+) {
+	public static PaperListResponse of(int paperCount, CursorPage<PaperResponse> rollingPapers) {
+		return new PaperListResponse(paperCount, rollingPapers);
+	}
+}
