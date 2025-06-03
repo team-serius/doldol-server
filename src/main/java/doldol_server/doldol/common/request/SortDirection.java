6d28d625409ab87a72a1182228587cf5b5e51378
cherryiJuice <chldlwn711@gmail.com>
2025-06-03 21:06:14 +0900

    feat: #44 커서페이징 필요 클래스 추가

---
+++ src/main/java/doldol_server/doldol/common/request/SortDirection.java
@@ -0,0 +1,6 @@
+package doldol_server.doldol.common.request;
+
+public enum SortDirection {
+	LATEST,  // 최신순 (id DESC)
+	OLDEST   // 오래된순 (id ASC)
+}
---
+++ src/main/java/doldol_server/doldol/common/response/ApiCursorPageResponse.java
@@ -0,0 +1,42 @@
+package doldol_server.doldol.common.response;
+
+import java.util.List;
+
+import doldol_server.doldol.common.dto.CursorPage;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class ApiCursorPageResponse<T, C> {
+
+	@Schema(description = "데이터")
+	private final List<T> data;
+
+	@Schema(description = "다음 커서")
+	private final C nextCursor;
+
+	@Schema(description = "다음 데이터 존재 여부")
+	private final boolean hasNext;
+
+	@Schema(description = "총 데이터 갯수 - 현재 페이지가 아닌 전체 데이터 갯수")
+	private final int totalPages;
+
+	@Schema(description = "응답 코드", example = "200")
+	private final int code;
+
+	@Schema(description = "응답 메세지", example = "success")
+	private final String message;
+
+	public ApiCursorPageResponse(CursorPage<T, C> data, int code, String message) {
+		this.data = data.getData();
+		this.nextCursor = data.getNextCursor();
+		this.hasNext = data.isHasNext();
+		this.totalPages = data.getData().size();
+		this.code = code;
+		this.message = message;
+	}
+
+	public static <T, C> ApiCursorPageResponse<T, C> ok(CursorPage<T, C> data) {
+		return new ApiCursorPageResponse<>(data, 200, "OK");
+	}
+}
