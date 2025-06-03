8ac046fefb9913cdeb1c2121737ac99b88db953f
cherryiJuice <chldlwn711@gmail.com>
2025-06-03 22:58:52 +0900

    refactor: #44 커서 데이터 타입 확정

--- src/main/java/doldol_server/doldol/common/dto/CursorPage.java
+++ src/main/java/doldol_server/doldol/common/dto/CursorPage.java
@@ -1,27 +1,22 @@
 package doldol_server.doldol.common.dto;
 
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.ToLongFunction;
 
 import lombok.Getter;
 
 @Getter
-public class CursorPage<T, C> {
+public class CursorPage<T> {
 	private final List<T> data;
 	private final int size;
-	private final C nextCursor;
+	private final Long nextCursor;
 	private final boolean hasNext;
 
-	private CursorPage(List<T> data, int requestedSize, Function<T, C> cursorGetter, boolean hasNext) {
-		if (hasNext) {
-			this.size = requestedSize;
-			this.nextCursor = cursorGetter.apply(data.get(this.size));
-		} else {
-			this.size = data.size();
-			this.nextCursor = null;
-		}
+	private CursorPage(List<T> data, int requestedSize, ToLongFunction<T> cursorGetter, boolean hasNext) {
+		this.size = hasNext ? requestedSize : data.size();
 		this.hasNext = hasNext;
 		this.data = data.subList(0, this.size);
+		this.nextCursor = hasNext ? cursorGetter.applyAsLong(data.get(this.size)) : null;
 	}
 
 	private CursorPage(List<T> data) {
@@ -31,7 +26,7 @@ public class CursorPage<T, C> {
 		this.nextCursor = null;
 	}
 
-	public static <T, C> CursorPage<T, C> of(List<T> data, int requestedSize, Function<T, C> cursorGetter) {
+	public static <T> CursorPage<T> of(List<T> data, int requestedSize, ToLongFunction<T> cursorGetter) {
 		if (data.isEmpty())
 			return new CursorPage<>(data);
 		if (data.size() == requestedSize + 1)
