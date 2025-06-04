3bd3e80025fb26c4be78a2c9c88ee337dac2787c
cherryiJuice <chldlwn711@gmail.com>
2025-06-04 13:19:28 +0900

    refactor: #44 참여자 조회 관련 쿼리 3개 -> 1개로 줄임

--- src/main/java/doldol_server/doldol/rollingPaper/repository/ParticipantRepository.java
+++ src/main/java/doldol_server/doldol/rollingPaper/repository/ParticipantRepository.java
@@ -1,9 +1,17 @@
 package doldol_server.doldol.rollingPaper.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import doldol_server.doldol.rollingPaper.entity.Participant;
-import doldol_server.doldol.rollingPaper.repository.custom.ParticipantRepositoryCustom;
 
-public interface ParticipantRepository extends JpaRepository<Participant, Long>, ParticipantRepositoryCustom {
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+	@Query("select p "
+		+ "from Participant p "
+		+ "join fetch p.paper pa "
+		+ "join fetch p.user u "
+		+ "where pa.id = :paperId")
+	List<Participant> findByIdWithUser(Long paperId);
 }
