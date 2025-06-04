a99192466761b78f1d4a57b93430e410752c9ccd
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-04 23:12:59 +0900

    test: #60 add mock CustomUserDetails

--- src/test/java/doldol_server/doldol/common/ControllerTest.java
+++ src/test/java/doldol_server/doldol/common/ControllerTest.java
@@ -1,12 +1,21 @@
 package doldol_server.doldol.common;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import doldol_server.doldol.auth.dto.CustomUserDetails;
 import doldol_server.doldol.common.config.TestSecurityConfig;
 
 @Import(TestSecurityConfig.class)
@@ -25,5 +34,16 @@ public abstract class ControllerTest {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	protected RequestPostProcessor mockUser(Long userId) {
+		CustomUserDetails userDetails = mock(CustomUserDetails.class);
+		when(userDetails.getUserId()).thenReturn(userId);
+
+		Authentication auth = new UsernamePasswordAuthenticationToken(
+			userDetails, null, List.of()
+		);
+
+		return authentication(auth);
 	}
 }
