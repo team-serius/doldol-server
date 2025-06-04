52280e3c70b23cdcff84a1d9b097e176b9b81607
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-04 19:52:45 +0900

    feat #11: 개인정보 수정 컨트롤러 메서드 생성

---
+++ src/main/java/doldol_server/doldol/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package doldol_server.doldol.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import doldol_server.doldol.auth.dto.CustomUserDetails;
+import doldol_server.doldol.common.response.ApiResponse;
+import doldol_server.doldol.user.dto.request.UpdateUserInfoRequest;
+import doldol_server.doldol.user.dto.response.UpdateUserInfoResponse;
+import doldol_server.doldol.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "사용자")
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping
+	@Operation(
+		summary = "개인정보 수정 API",
+		description = "개인정보 수정",
+		security = {@SecurityRequirement(name = "jwt")})
+	public ResponseEntity<ApiResponse<UpdateUserInfoResponse>> updateUserInfo(
+		@RequestBody @Valid UpdateUserInfoRequest request,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		UpdateUserInfoResponse response = userService.changeInfo(request, userDetails.getUserId());
+		return ResponseEntity.ok(ApiResponse.ok(response));
+	}
+}
