8650e6a6eded050246917b5d90c506c2b1795a63
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-04 19:51:21 +0900

    feat #11: UserService changeInfo 임시 함수 생성

--- src/main/java/doldol_server/doldol/user/service/UserService.java
+++ src/main/java/doldol_server/doldol/user/service/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import doldol_server.doldol.common.exception.CustomException;
 import doldol_server.doldol.common.exception.errorCode.UserErrorCode;
+import doldol_server.doldol.user.dto.request.UpdateUserInfoRequest;
+import doldol_server.doldol.user.dto.response.UpdateUserInfoResponse;
 import doldol_server.doldol.user.entity.User;
 import doldol_server.doldol.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +20,10 @@ public class UserService {
 
 	public User getById(Long userId) {
 		return userRepository.findById(userId).orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+	}
+
+	public UpdateUserInfoResponse changeInfo(UpdateUserInfoRequest request, Long userId) {
+		UpdateUserInfoResponse updateUserInfoResponse = new UpdateUserInfoResponse(request.name(), request.password());
+		return updateUserInfoResponse;
 	}
 }
