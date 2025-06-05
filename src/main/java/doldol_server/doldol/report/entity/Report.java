d9e3b52614af6af7c14e86e80e5a8ea4d28a52ec
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-05 17:02:28 +0900

    refactor: #66 Add builder support to Report entity for testing

--- src/main/java/doldol_server/doldol/report/entity/Report.java
+++ src/main/java/doldol_server/doldol/report/entity/Report.java
@@ -13,11 +13,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Report extends BaseEntity {
 
