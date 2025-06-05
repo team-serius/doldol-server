0f108d9832058f20157705d9c5a6d8cd6983d18d
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-05 19:27:55 +0900

    feat #11: updateUserInfo 작성을 위한 수정

--- src/main/java/doldol_server/doldol/user/dto/request/UpdateUserInfoRequest.java
+++ src/main/java/doldol_server/doldol/user/dto/request/UpdateUserInfoRequest.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.Pattern;
 @Schema(name = "UpdateUserInfoRequest: 개인정보 수정 요청 Dto")
 public record UpdateUserInfoRequest(
 
-	@Schema(description = "변경할 이름", example = "김돌돌")
+	@Schema(description = "변경할 이름", example = "김둘둘")
 	String name,
 
 	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$",
 		message = "비밀번호는 8~16자리로 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.")
-	@Schema(description = "변경할 비밀번호", example = "doldol1234!")
+	@Schema(description = "변경할 비밀번호", example = "doldol2345!")
 	String password) {
 
 }
