6ce22528216f2bdb830e263fd06e1618a1ca47f8
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-06 22:14:39 +0900

    refactor: #66 Eliminate unnecessary Optional wrapping of fetchOne

--- src/main/java/doldol_server/doldol/report/repository/custom/ReportRepositoryCustom.java
+++ src/main/java/doldol_server/doldol/report/repository/custom/ReportRepositoryCustom.java
@@ -1,11 +1,10 @@
 package doldol_server.doldol.report.repository.custom;
 
 import java.util.List;
-import java.util.Optional;
 
 import doldol_server.doldol.report.entity.Report;
 
 public interface ReportRepositoryCustom {
 	List<Report> findReportsByUserId(Long userId);
-	Optional<Report> findByIdAndUserId(Long reportId, Long userId);
+	Report findByIdAndUserId(Long reportId, Long userId);
 }
