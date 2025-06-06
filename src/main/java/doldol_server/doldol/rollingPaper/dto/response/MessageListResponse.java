b708260ec45950a235454d1b01da2ee419de45ba
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-06 17:45:54 +0900

    refactor: #69 return cursor date on message response

--- src/main/java/doldol_server/doldol/rollingPaper/dto/response/MessageListResponse.java
+++ src/main/java/doldol_server/doldol/rollingPaper/dto/response/MessageListResponse.java
@@ -1,20 +1,16 @@
 package doldol_server.doldol.rollingPaper.dto.response;
 
-import java.util.List;
-
+import doldol_server.doldol.common.dto.CursorPage;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "MessageListResponse: 메세지 리스트 응답 Dto")
 public record MessageListResponse(
-	@Schema(description = "메세지 갯수", example = "24")
+	@Schema(description = "총 메세지 수", example = "24")
 	int messageCount,
 
-	@Schema(description = "롤링페이퍼 url", example = "https://www.doldol.com/{uuid}")
-	String url,
-
-	@Schema(description = "관리자 여부", example = "true")
-	boolean isMaster,
-
-	List<MessageResponse> message
+	CursorPage<MessageResponse> message
 ) {
+	public static MessageListResponse of(int messageCount, CursorPage<MessageResponse> message) {
+		return new MessageListResponse(messageCount, message);
+	}
 }
