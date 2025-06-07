93c576104a2e2a784cc468716091d53834365011
Hoonssac <Hoonssac.dev@gmail.com>
2025-06-07 22:25:05 +0900

    feat: #66 Add validation to ensure only message recipients can create a report

--- src/main/java/doldol_server/doldol/common/exception/errorCode/ReportErrorCode.java
+++ src/main/java/doldol_server/doldol/common/exception/errorCode/ReportErrorCode.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ReportErrorCode implements ErrorCode {
 	REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "P-001", "해당 신고 내역을 찾을 수 없습니다."),
-	REPORT_FORBIDDIN(HttpStatus.FORBIDDEN, "P-002", "해당 신고 내역에 접근할 권한이 없습니다.");
+	REPORT_FORBIDDEN(HttpStatus.FORBIDDEN, "P-002", "해당 신고 내역에 접근할 권한이 없습니다.");
 
 	private HttpStatus httpStatus;
 	private String code;
