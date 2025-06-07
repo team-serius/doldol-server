0cf902be5c21f376034b7f6e94f6ec3393e7dc32
cherryiJuice <chldlwn711@gmail.com>
2025-06-08 00:19:22 +0900

    fix: #77 헤더 오타 수정

--- src/main/java/doldol_server/doldol/common/constants/CorsConstant.java
+++ src/main/java/doldol_server/doldol/common/constants/CorsConstant.java
@@ -10,7 +10,7 @@ public class CorsConstant {
 	public static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:3000", "http://localhost:3001",
 		"https://dev.doldol.wha1eson.co.kr");
 	public static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	public static final List<String> ALLOWED_HEADERS = List.of("X-Requested-With, Content-Type, Accept");
+	public static final List<String> ALLOWED_HEADERS = List.of("X-Requested-With", "Content-Type", "Accept");
 	public static final boolean ALLOWED_CREDENTIALS = true;
 	public static final long MAX_AGE = 3600L;
 }
