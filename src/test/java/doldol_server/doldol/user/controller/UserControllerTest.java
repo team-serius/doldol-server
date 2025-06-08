0d34bf440848520c55975fa4f7e5a8baae2b11ba
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-09 03:37:05 +0900

    test: #87 getMyInfo test 추가

--- src/test/java/doldol_server/doldol/user/controller/UserControllerTest.java
+++ src/test/java/doldol_server/doldol/user/controller/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import doldol_server.doldol.common.ControllerTest;
 import doldol_server.doldol.user.dto.request.UpdateUserInfoRequest;
+import doldol_server.doldol.user.dto.response.UserResponse;
 import doldol_server.doldol.user.service.UserService;
 
 @WebMvcTest(controllers = UserController.class)
@@ -37,5 +38,33 @@ class UserControllerTest extends ControllerTest {
 			.andExpect(jsonPath("$.status").value(204));
 
 		verify(userService).changeInfo(any(UpdateUserInfoRequest.class), anyLong());
+	}
+
+	@Test
+	@DisplayName("사용자 정보 조회 - 성공")
+	void getMyInfo_Success() throws Exception {
+		// given
+		UserResponse mockResponse = UserResponse.builder()
+			.name("김돌돌")
+			.phone("01012341234")
+			.email("doldol@test.com")
+			.socialId("1233244124")
+			.socialType("kakao")
+			.build();
+
+		when(userService.getMyInfo(1L)).thenReturn(mockResponse);
+
+		// when & then
+		mockMvc.perform(get("/user/me")
+				.with(mockUser(1L)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.status").value(200))
+			.andExpect(jsonPath("$.data.name").value("김돌돌"))
+			.andExpect(jsonPath("$.data.email").value("doldol@test.com"))
+			.andExpect(jsonPath("$.data.phone").value("01012341234"))
+			.andExpect(jsonPath("$.data.socialType").value("kakao"));
+
+		// verify
+		verify(userService).getMyInfo(1L);
 	}
 }
