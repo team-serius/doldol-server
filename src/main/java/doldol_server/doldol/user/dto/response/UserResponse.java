f446f551550ca90d04878940abd7d251115bcdd5
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-09 03:15:23 +0900

    feat: #87 GET ME 요청을 위한 Response DTO 생성

---
+++ src/main/java/doldol_server/doldol/user/dto/response/UserResponse.java
@@ -0,0 +1,28 @@
+package doldol_server.doldol.user.dto.response;
+
+import doldol_server.doldol.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "MyInfo Response: 사용자 본인 정보 응답 Dto")
+public record UserResponse(
+	@Schema(description = "이름", example = "김돌돌")
+	String name,
+
+	@Schema(description = "휴대전화 번호", example = "01012341234")
+	String phone,
+
+	@Schema(description = "이메일", example = "doldol@test.com")
+	String email,
+
+	@Schema(description = "소셜 아이디", example = "1233244124")
+	String socialId,
+
+	@Schema(description = "소셜 타입", example = "kakao")
+	String socialType
+
+) {
+	public static UserResponse of(User user) {
+		return new UserResponse(user.getName(), user.getPhone(), user.getEmail(),
+			user.getSocialId(), user.getSocialType().name().toLowerCase());
+	}
+}
