adaba756a1417c4cf607254b57843982e75b7a1d
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-14 19:30:43 +0900

    refactor: #112 delete cookie util

--- src/main/java/doldol_server/doldol/common/constants/CookieConstant.java
+++
@@ -1,7 +0,0 @@
-package doldol_server.doldol.common.constants;
-
-public class CookieConstant {
-    public static final String ACCESS_TOKEN_COOKIE_NAME = "Access-Token";
-    public static final String REFRESH_TOKEN_COOKIE_NAME = "Refresh-Token";
-    public static final int TOKEN_EXPIRATION_DELETE = 0;
-}
