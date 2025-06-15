beb17596d896ee47e61acf7a911facf862a76e7f
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-15 21:55:56 +0900

    fix: #114 extend AT expiration time

--- src/main/java/doldol_server/doldol/common/constants/TokenConstant.java
+++ src/main/java/doldol_server/doldol/common/constants/TokenConstant.java
@@ -4,6 +4,6 @@ public class TokenConstant {
 
 	public static final int MINUTE_IN_MILLISECONDS = 60 * 1000;
 	public static final long DAYS_IN_MILLISECONDS = 24 * 60 * 60 * 1000L;
-	public static final int ACCESS_TOKEN_EXPIRATION_MINUTE = 1;
+	public static final int ACCESS_TOKEN_EXPIRATION_MINUTE = 20;
 	public static final int REFRESH_TOKEN_EXPIRATION_DAYS = 14;
 }
