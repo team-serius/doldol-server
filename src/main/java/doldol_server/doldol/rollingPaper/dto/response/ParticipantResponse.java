e2e56c630d2421df111b0525fbcfbbae3a7b66f8
cherryiJuice <chldlwn711@gmail.com>
2025-06-17 17:43:47 +0900

    feat: #125 append phone suffix to ParticipantResponse name

--- src/main/java/doldol_server/doldol/rollingPaper/dto/response/ParticipantResponse.java
+++ src/main/java/doldol_server/doldol/rollingPaper/dto/response/ParticipantResponse.java
@@ -1,6 +1,7 @@
 package doldol_server.doldol.rollingPaper.dto.response;
 
 import doldol_server.doldol.rollingPaper.entity.Participant;
+import doldol_server.doldol.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "ParticipantResponse: 롤링페이퍼 참여자 응답 Dto")
@@ -11,11 +12,18 @@ public record ParticipantResponse(
 	@Schema(description = "참여 유저 아이디", example = "1")
 	Long userId,
 
-	@Schema(description = "참여자 이름", example = "김돌돌")
+	@Schema(description = "참여자 이름", example = "김돌돌(1234)")
 	String name
 ) {
 	public static ParticipantResponse of(Participant participant) {
-		return new ParticipantResponse(participant.getId(), participant.getUser().getId(),
-			participant.getUser().getName());
+		User user = participant.getUser();
+		String lastFourDigits = user.getPhone().substring(user.getPhone().length() - 4);
+		String displayName = user.getName() + " (" + lastFourDigits + ")";
+
+		return new ParticipantResponse(
+			participant.getId(),
+			user.getId(),
+			displayName
+		);
 	}
 }
