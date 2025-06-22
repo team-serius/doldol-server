db167667670da94f2a430126b799fecbfb0d98cd
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-22 23:37:01 +0900

    fix: #151 can't send message to same user

--- src/main/java/doldol_server/doldol/rollingPaper/repository/MessageRepository.java
+++ src/main/java/doldol_server/doldol/rollingPaper/repository/MessageRepository.java
@@ -3,7 +3,10 @@ package doldol_server.doldol.rollingPaper.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import doldol_server.doldol.rollingPaper.entity.Message;
+import doldol_server.doldol.rollingPaper.entity.Paper;
 import doldol_server.doldol.rollingPaper.repository.custom.MessageRepositoryCustom;
+import doldol_server.doldol.user.entity.User;
 
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
+	boolean existsByPaperAndFromAndToAndIsDeletedFalse(Paper paper, User from, User to);
 }
