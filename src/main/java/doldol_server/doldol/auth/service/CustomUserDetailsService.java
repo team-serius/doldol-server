6ac37c09eca97998a879bcecf8b4bc2431cf1bbc
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-27 22:38:30 +0900

    test: #196 check loginId

--- src/main/java/doldol_server/doldol/auth/service/CustomUserDetailsService.java
+++ src/main/java/doldol_server/doldol/auth/service/CustomUserDetailsService.java
@@ -11,7 +11,9 @@ import doldol_server.doldol.auth.dto.CustomUserDetails;
 import doldol_server.doldol.user.entity.User;
 import doldol_server.doldol.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,10 +22,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public CustomUserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        log.info("id: {}", loginId);
         Optional<User> user = userRepository.findByLoginIdAndIsDeletedFalse(loginId);
 
         if (user.isPresent()) {
             User loginUser = user.get();
+            log.info("user id: {}", loginUser.getLoginId());
             return new CustomUserDetails(loginUser);
         }
         return null;
