acbfddaa8daf1122ec15a87eb00140ba65281c95
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-27 17:35:00 +0900

    test: #190 calculate time

--- src/main/java/doldol_server/doldol/auth/service/TestService.java
+++ src/main/java/doldol_server/doldol/auth/service/TestService.java
@@ -9,7 +9,9 @@ import doldol_server.doldol.auth.util.EncryptionUtil;
 import doldol_server.doldol.user.entity.User;
 import doldol_server.doldol.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TestService {
@@ -18,7 +20,9 @@ public class TestService {
 	private final UserRepository userRepository;
 
 	public Map<String, String> test(String email) {
-		User byEmail = userRepository.findByEmail(encryptionUtil.encrypt(email));
+		String encrypt = encryptionUtil.encrypt(email);
+		log.info("email: {}", encrypt);
+		User byEmail = userRepository.findByEmail(encrypt);
 		Map<String, String> result = new HashMap<>();
 		result.put("email", encryptionUtil.decrypt(byEmail.getEmail()));
 
