e624b5251eb0012d9f2de61003312905af8dd78f
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-30 22:59:24 +0900

    feat: 207 add white list

--- src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
+++ src/main/java/doldol_server/doldol/common/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
 		"/swagger-resources/**",
 		"/webjars/**",
 		"/actuator/**",
-		"/papers/invite"
+		"/papers/invite",
+		"/messages/individual"
 	};
 
 	private static final String[] BLACKLIST = {
