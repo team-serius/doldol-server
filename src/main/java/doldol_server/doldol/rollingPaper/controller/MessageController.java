b0421d2e9bce5c1a5412c888a26467f73d7f6ffd
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-30 22:57:07 +0900

    choir: 207 delete unused import

--- src/main/java/doldol_server/doldol/rollingPaper/controller/MessageController.java
+++ src/main/java/doldol_server/doldol/rollingPaper/controller/MessageController.java
@@ -1,7 +1,5 @@
 package doldol_server.doldol.rollingPaper.controller;
 
-import java.time.LocalDate;
-
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
