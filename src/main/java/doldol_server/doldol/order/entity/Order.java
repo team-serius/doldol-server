e5653f54fbb9b6b8f229c1a72bcc1849e4066397
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-01 11:24:20 +0900

    fix: 209 set order's table name

--- src/main/java/doldol_server/doldol/order/entity/Order.java
+++ src/main/java/doldol_server/doldol/order/entity/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order extends BaseEntity {
 
