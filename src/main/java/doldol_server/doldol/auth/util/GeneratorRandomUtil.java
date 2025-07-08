b23059131eb4acbe8477c8e9de5684431435ab4f
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-08 16:21:57 +0900

    fix: 216 generate temp password with password pattern

--- src/main/java/doldol_server/doldol/auth/util/GeneratorRandomUtil.java
+++ src/main/java/doldol_server/doldol/auth/util/GeneratorRandomUtil.java
@@ -11,13 +11,33 @@ public class GeneratorRandomUtil {
 	private static final SecureRandom RANDOM = new SecureRandom();
 
 	public static String generateRandomString() {
+		String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lowercase = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String specialChars = "$@!%*#?&";
 
-		StringBuilder sb = new StringBuilder(10);
-		for (int i = 0; i < 10; i++) {
-			char randomChar = (char) (RANDOM.nextInt(94) + 33);
-			sb.append(randomChar);
+		int length = 8 + RANDOM.nextInt(9);
+		StringBuilder password = new StringBuilder(length);
+
+		password.append(uppercase.charAt(RANDOM.nextInt(uppercase.length())));
+		password.append(lowercase.charAt(RANDOM.nextInt(lowercase.length())));
+		password.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
+		password.append(specialChars.charAt(RANDOM.nextInt(specialChars.length())));
+
+		String allChars = uppercase + lowercase + numbers + specialChars;
+		for (int i = 4; i < length; i++) {
+			password.append(allChars.charAt(RANDOM.nextInt(allChars.length())));
 		}
-		return sb.toString();
+
+		char[] chars = password.toString().toCharArray();
+		for (int i = chars.length - 1; i > 0; i--) {
+			int j = RANDOM.nextInt(i + 1);
+			char temp = chars[i];
+			chars[i] = chars[j];
+			chars[j] = temp;
+		}
+
+		return new String(chars);
 	}
 
 	public static String generateRandomNum() {
