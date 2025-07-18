ea0b176c6977f40e21aa4c0e82fe4173d9f9d44e
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-18 20:34:14 +0900

    refactor: 219 add prod profile on swagger config

--- src/main/java/doldol_server/doldol/common/config/SwaggerConfig.java
+++ src/main/java/doldol_server/doldol/common/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local", "dev"})
+@Profile({"local", "dev", "prod"})
 public class SwaggerConfig {
 
 	@Bean
