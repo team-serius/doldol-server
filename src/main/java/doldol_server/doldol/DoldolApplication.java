d8d629f6edb320382019451dfa217d4a094dc4d6
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-16 01:31:23 +0900

    refactor: #242 delete aop annotation

--- src/main/java/doldol_server/doldol/DoldolApplication.java
+++ src/main/java/doldol_server/doldol/DoldolApplication.java
@@ -3,10 +3,8 @@ package doldol_server.doldol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableFeignClients
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class DoldolApplication {
 
