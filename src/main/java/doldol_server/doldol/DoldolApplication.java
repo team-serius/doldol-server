7cbd3d5ea1f8be918e4197c2aff7330e10e079c4
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-15 17:21:07 +0900

    fix: #242 specify aop

--- src/main/java/doldol_server/doldol/DoldolApplication.java
+++ src/main/java/doldol_server/doldol/DoldolApplication.java
@@ -3,8 +3,10 @@ package doldol_server.doldol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableFeignClients
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class DoldolApplication {
 
